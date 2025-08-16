842c46d234561f28af2a17e4751224ea1fd48d22
Rekhta Menahil <rekhta674@gmail.com>
2025-08-16 21:23:49 +0500

    add user and tenant types

--- types.ts
+++ types.ts
@@ -2,3 +2,18 @@ export type Credentails={
     email: string
     password: string
 }
+
+export type User = {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    createdAt: string;
+    tenant: Tenant | null;
+};
+
+export type Tenant = {
+    id: number;
+    name: string;
+    address: string;
+};
