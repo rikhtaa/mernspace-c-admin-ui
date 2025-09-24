454a29e58683f5eaaefe4e534833ea55f1ae9ab8
Rekhta Menahil <rekhta674@gmail.com>
2025-09-24 16:19:00 +0500

    add tenant column

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -39,6 +39,16 @@ const columns = [
     title: 'Role',
     dataIndex: 'role',
     key: 'role' 
+  },
+  {
+    title: 'Restaurant',
+    dataIndex: 'tenant',
+    key: 'tenant',
+    render: (_text: string, record: User)=>{
+      return (
+        <div>{record.tenant?.name}</div>
+      )
+    }
   }
 ];
 const Users = () => {
