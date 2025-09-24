af120fdb1cccd1daf87294647fba0577eab334e3
Rekhta Menahil <rekhta674@gmail.com>
2025-09-24 21:17:16 +0500

    hide restaurants for manager users

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -20,11 +20,6 @@ const getMenuItems = (role: string)=>{
     label: <NavLink to='/'>Home</NavLink>
   },
   {
-    key: '/restaurants',
-    icon: <Icon component={foodIcon}/>,
-    label: <NavLink to='/restaurants'>Restaurants</NavLink>
-  },
-  {
     key: '/products',
     icon: <Icon component={Product}/>,
     label: <NavLink to='/products'>Products</NavLink>
@@ -41,6 +36,11 @@ const getMenuItems = (role: string)=>{
     key: '/users',
     icon: <Icon component={UserIcon}/>,
     label: <NavLink to='/users'>Users</NavLink>
+    })
+    menus.splice(2,0, {
+    key: '/restaurants',
+    icon: <Icon component={foodIcon}/>,
+    label: <NavLink to='/restaurants'>Restaurants</NavLink>
     })
     return menus
    }
